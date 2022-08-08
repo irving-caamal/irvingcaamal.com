f98411c8beeb1215cc29d8d366ee55f6773b7a63
Irving <irvv.17@gmail.com>
2022-08-07 23:34:13 -0500

    chore(add tailwindcss packages): also add config files (#3)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { BaseLayout } from "../layouts/base";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <BaseLayout>
+        <Component {...pageProps} />
+      </BaseLayout>
+    </>
+  );
 }
 
 export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return <h1>Hi!</h1>;
+};
+
+export default Home;
