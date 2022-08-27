b50e176353e1d29bb4d0a2bfee63b72850c78d6f
irvv17 <irvv.17@gmail.com>
2022-08-26 23:41:57 -0500

    fix(linter): issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/layouts/*.{js,ts,jsx,tsx}",
+    "./src/layouts/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
