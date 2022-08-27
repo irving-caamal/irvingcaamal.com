272c4da2e60a2b732664b44687fb4f0534f80f22
irvv17 <irvv.17@gmail.com>
2022-08-27 16:47:13 -0500

    fix(minor adjusments in e2e tests): fix lint, fix the way we test the title of the page(s)

--- src/tests/e2e/homepage.spec.ts
+++ src/tests/e2e/homepage.spec.ts
@@ -4,22 +4,22 @@ test("should render HomePage", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("http://localhost:3000/");
   // The new page should contain an h1
-  await expect(page.locator("h1")).toContainText(
-    "Hi, I'm Irving Caamal"
-  );
+  await expect(page.locator("h1")).toContainText("Hi, I'm Irving Caamal");
   // The new page should contain an h2
   await expect(page.locator("h2")).toContainText(
     "Systems Engineer / Full Stack Web Developer - Focused on FrontEnd Development"
   );
 });
 
-test('should navigate to the stack page', async ({ page }) => {
+test("should navigate to the stack page", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('http://localhost:3000/')
+  await page.goto("http://localhost:3000/");
   // Find an element with the text 'Technologies I'm currently working with' and click on it
-  await page.click(`text=Technologies I'm currently working with`)
+  await page.click(`text=Technologies I'm currently working with`);
   // The new url should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL('http://localhost:3000/stack')
+  await expect(page).toHaveURL("http://localhost:3000/stack");
   // The new page should contain the h1 according to the route
-  await expect(page.locator('h1')).toContainText(`Currently Stack I'm working with`)
-})
+  await expect(page.locator("h1")).toContainText(
+    `Currently Stack I'm working with`
+  );
+});
