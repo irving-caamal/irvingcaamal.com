cff076bdab23c569d98b8d319f3e60fb347753b8
irvv17 <irvv.17@gmail.com>
2022-08-27 18:27:05 -0500

    fix(heading): fix heading set h1 instead of h2

--- src/pages/stack.tsx
+++ src/pages/stack.tsx
@@ -3,10 +3,11 @@ import { Stack } from "../utils/Stack";
 function stack() {
   return (
     <div className="flex flex-col justify-center items-start gap-x-4 w-full px:4 md:px-12">
+      <h1 className="text-6xl mb-5">Currently Stack I&apos;m working with</h1>
       <div className="flex flex-col">
         {Stack.map((item, index) => (
           <>
-            <h1 className="text-6xl mb-5">{item.title}</h1>
+            <h2 className="text-6xl mb-5">{item.title}</h2>
             <ul className="flex space-x-2">
               {item.items &&
                 item.items.map((item, index) => (
