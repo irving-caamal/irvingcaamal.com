9e178550224df3bb2f1ba8f2d409e93a62576318
Irving <irvv.17@gmail.com>
2022-08-27 23:48:09 -0500

    ci(add e2e pipeline): pipeline that runs e2e tests (#12)

--- src/tests/e2e/homepage.spec.ts
+++ src/tests/e2e/homepage.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("should render HomePage", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
   // The new page should contain an h1
   await expect(page.locator("h1")).toContainText("Hi, I'm Irving Caamal");
   // The new page should contain an h2
@@ -13,11 +13,11 @@ test("should render HomePage", async ({ page }) => {
 
 test("should navigate to the stack page", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
   // Find an element with the text 'Technologies I'm currently working with' and click on it
   await page.click(`text=Technologies I'm currently working with`);
   // The new url should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL("http://localhost:3000/stack");
+  await expect(page).toHaveURL("/stack");
   // The new page should contain the h1 according to the route
   await expect(page.locator("h1")).toContainText(
     `Currently Stack I'm working with`
--- src/tests/e2e/stack.spec.ts
+++ src/tests/e2e/stack.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("should render Stack Page", async ({ page }) => {
-  await page.goto("http://localhost:3000/stack");
+  await page.goto("/stack");
   await expect(page.locator("h1")).toContainText(
     "Currently Stack I'm working with"
   );
