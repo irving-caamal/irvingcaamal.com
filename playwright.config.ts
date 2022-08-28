8ba591dcaeb02b8da7cd86f9bcfbf33e6b341781
irvv17 <irvv.17@gmail.com>
2022-08-27 23:39:14 -0500

    fix(linter): fix linter issues

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ const config: PlaywrightTestConfig = {
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: "on-first-retry",
-    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000'
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000"
   },
   testDir: "./src/tests/e2e"
 };
