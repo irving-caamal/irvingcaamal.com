0b941db84b67c77505cc7377919ee5a23ce122f5
irvv17 <irvv.17@gmail.com>
2022-08-27 23:38:03 -0500

    fix(add baseurl to playwright config): add baseURL param

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,8 @@ const config: PlaywrightTestConfig = {
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
-    video: "on-first-retry"
+    video: "on-first-retry",
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000'
   },
   testDir: "./src/tests/e2e"
 };
