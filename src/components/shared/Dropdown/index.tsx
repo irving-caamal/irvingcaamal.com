74093f5d69a5227db2c4c620e32d40274eea2583
irvv17 <irvv.17@gmail.com>
2022-11-13 23:38:25 -0500

    add dropdown component

---
+++ src/components/shared/Dropdown/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+
+export const Dropdown = ({ trigger, children }: { trigger: JSX.Element, children: JSX.Element }) => {
+  return (
+    <>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+          {trigger}
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content>
+          {children}
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+    </>
+  )
+}
