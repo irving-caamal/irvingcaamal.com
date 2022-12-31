5f0fc0356b184bbd30a88b2801fb144f1d47176f
Irving Caamal <irvv.17@gmail.com>
2022-12-31 14:37:48 -0500

    fix(linter): fix entire file

--- src/pages/404.page.tsx
+++ src/pages/404.page.tsx
@@ -1,23 +1,21 @@
 import Link from "next/link";
-import { Home } from "react-feather";
+import {Home} from "react-feather";
 
 export default function FourOhFour() {
-  return (
-    <div className="flex flex-col text-2xl">
-      <h1>
-        This is embarrassing, we couldn&apos;t find what you want, but you can
-        try other resources:
-      </h1>
-      <ul>
-        <li>
-          <Link href="/" passHref>
-            <a className="flex items-center">
-              <Home size={24} />
-              &nbsp;Go back home
-            </a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className="flex flex-col text-2xl">
+            <h1>
+                This is embarrassing, we couldn&apos;t find what you want, but you can
+                try other resources:
+            </h1>
+            <ul>
+                <li>
+                    <Link href="/" passHref className="flex items-center">
+                        <Home size={24}/>
+                        &nbsp;Go back home
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
