6f318dfe4a4b96decd1da76d36554670eea4ac40
Irving Caamal <irvv.17@gmail.com>
2023-04-26 12:32:54 -0500

    fix(more linter): issues

--- next.config.js
+++ next.config.js
@@ -4,14 +4,13 @@ const nextConfig = {
   swcMinify: true,
   webpack(config) {
     config.module.rules.push({
-
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"]
     });
 
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
