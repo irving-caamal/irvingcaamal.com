600e0889179c34261e7f9151a5916ad93009339d
Irving Caamal <contact@irvingcaamal.com>
2026-01-14 18:24:38 -0500

    Icaamal/enhance seo updates (#66)
    * chore(upgrade): many dependencies and subdependencies
    
    * fix(styles): config update to tailwind v4
    
    * Delete globals.css
    
    * feat(seo): enhancements and updates
    
    * Update pnpm-lock.yaml
    
    * fix(lint): and format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   integrations: [
     react(),
+    sitemap(),
   ],
   output: 'static',
   site: 'https://irvingcaamal.com',
